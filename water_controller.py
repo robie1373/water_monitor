76aa965aaaa3ac54846c23faea8092a6aafd2bdf
Robie Lutsey <robie1373@gmail.com>
2015-03-08 01:52:14 -0500

    first commit

---
+++ water_controller.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+
+## set up pins
+# input pin for flow sensor
+
+# input pin for manual override
+
+# output pin for solenoid
+
+## use GPIO callback to detect water flow
+
+## calculate flowrate (if useful)
+
+## calculate total flow over 5 minutes
+
+## Home mode (do not turn off water)
+
+## Away mode (turn off water if flow over 5 minutes exceeds threshold)
+
+## Persist the data for logging
+
+## Present the data and config for web interface
+
+## Manual override (turn water on or off? with a switch)
