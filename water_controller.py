76206ea0ae63c2524026455075e7e6ac6a6202e0
Robie Lutsey <robie1373@gmail.com>
2015-03-09 01:14:08 -0400

    main code seems to work.

--- water_controller.py
+++ water_controller.py
@@ -7,12 +7,12 @@ from threading import Timer
 import time
 
 GPIO.setmode(GPIO.BOARD)
-
+GPIO.setwarnings(False)
 ## set up pins
 # input pin for flow sensor
 
 flow_sensor = 7
-GPIO.setup(flow_sensor, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(flow_sensor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # input pin for manual override
 
@@ -29,6 +29,7 @@ flow_ticks = 0
 def flow_rate_callback(flow_sensor):
   global flow_ticks
   flow_ticks += 1
+  print "event was detected. flow_ticks: ", flow_ticks
 
 GPIO.add_event_detect(flow_sensor, GPIO.RISING, callback=flow_rate_callback, bouncetime=100)
 
@@ -36,29 +37,34 @@ GPIO.add_event_detect(flow_sensor, GPIO.RISING, callback=flow_rate_callback, bou
 
 ## calculate total flow over 5 minutes
 ### time frame for the moving average in seconds
-moving_avg_time_frame = 300
+moving_avg_time_frame = 10
 
 ### Interval to take measurements in seconds
-reading_interval = 5
-readings = []
+reading_interval = 1
+readings = [0]
 
-def take_reading(readings):
+def take_reading():
+  print "taking a reading"
+  global flow_ticks
+  global readings
   if len(readings) > int(moving_avg_time_frame / reading_interval):
-    global flow_ticks
     # drop first reading from array
     readings.pop(0)
   # add new reading to end of array
   readings.append(flow_ticks)
   flow_ticks = 0
+  print "readings: ", readings, "flow_ticks: ", flow_ticks
 
-def threaded_readings(interval, readings):
-  Timer(interval, take_reading(readings), ()).start()
+def threaded_readings(interval):
+  Timer(interval, take_reading, ()).start()
 
 def add(x,y):
   return x+y
   
-def calculate_average(readings):
-  return reduce(add, readings)
+def calculate_average():
+  print "calculating the average of ", readings
+  return reduce(add, readings)/len(readings)
+
 ## Home mode (do not turn off water)
 
 ## Away mode (turn off water if flow over 5 minutes exceeds threshold)
@@ -70,17 +76,12 @@ def calculate_average(readings):
 ## Manual override (turn water on or off? with a switch)
 
 # Main loop
-threaded_readings(reading_interval, readings)
+while true:
+  threaded_readings(reading_interval, readings)
 
 #testing code
-print "First calculation"
-print readings
-print calculate_average(readings)
-time.sleep(50)
-print "Second calculation"
-print readings
-print calculate_average(readings)
-time.sleep(50)
-print "third calculation"
-print readings
-print calculate_average(readings)
+  print "Calculation #", x
+  print readings
+  print calculate_average()
+  time.sleep(5)
+  x += 1
