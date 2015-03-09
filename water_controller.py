dc3354c419f183da0bdb2bbd3d6252b3c9c6d264
Robie Lutsey <robie1373@gmail.com>
2015-03-08 23:37:55 -0400

    squashing more

--- water_controller.py
+++ water_controller.py
@@ -27,9 +27,9 @@ GPIO.setup(solenoid, GPIO.OUT)
 ## use GPIO callback to detect water flow
 flow_ticks = 0
 def flow_rate_callback(flow_sensor, flow_ticks):
-  flow_ticks += 1
+  global flow_ticks += 1
 
-GPIO.add_event_detect(flow_sensor, GPIO.RISING, callback=flow_rate_callback(flow_sensor, flow_ticks), bouncetime=100)
+GPIO.add_event_detect(flow_sensor, GPIO.RISING, callback=flow_rate_callback, bouncetime=100)
 
 ## calculate flowrate (if useful)
 
