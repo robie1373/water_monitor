e2a47f3f90a87f9dbc8c9231fb2ea11aee056041
Robie Lutsey <robie1373@gmail.com>
2015-03-08 21:45:26 -0400

    pins set up

--- water_controller.py
+++ water_controller.py
@@ -1,13 +1,29 @@
-import RPi.GPIO as GPIO
+try:
+  import RPi.GPIO as GPIO
+except RuntimeError:
+  print("Error importing RPi.GPIO! This is probably because you need superuser privileges. You can achieve this by using 'sudo' to run your script")
+
+GPIO.setmode(GPIO.BOARD)
 
 ## set up pins
 # input pin for flow sensor
 
+flow_sensor = 7
+GPIO.setup(flow_sensor, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 # input pin for manual override
+
+override = 11
+GPIO.setup(override, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # output pin for solenoid
 
+solenoid = 12
+GPIO.setup(solenoid, GPIO.OUT)
+
 ## use GPIO callback to detect water flow
+def flow_rate_callback(flow_sensor):
+  # put flow rate counter here
 
 ## calculate flowrate (if useful)
 
