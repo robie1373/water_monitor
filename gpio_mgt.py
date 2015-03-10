d147cc7e46581deec6044e6d0333895dd8058a99
Robie Lutsey <robie1373@gmail.com>
2015-03-09 21:37:47 -0400

    put main in class and GPIO bugs 08

--- gpio_mgt.py
+++ gpio_mgt.py
@@ -1,7 +1,7 @@
 try:
-    import RPi.GPIO as GPIO
-  except RuntimeError:
-    print("Error importing RPi.GPIO! This is probably because you need superuser privileges. You can achieve this by using 'sudo' to run your script")
+  import RPi.GPIO as GPIO
+except RuntimeError:
+  print("Error importing RPi.GPIO! This is probably because you need superuser privileges. You can achieve this by using 'sudo' to run your script")
 
 class GPIOManagement():
 
