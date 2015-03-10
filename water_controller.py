106c659cd740e190a337c0d0bbb63a3a3bfa2b3b
Robie Lutsey <robie1373@gmail.com>
2015-03-10 08:02:03 -0400

    looking for issues in main. trying to find helpful tests. 03

--- water_controller.py
+++ water_controller.py
@@ -41,7 +41,13 @@ class Main():
     self._flow_counter.flow_ticks = 0
 
     if platform == "rpi":
-      self._gpio                  = GPIOManagement()
+      try:
+        self._gpio                  = GPIOManagement()
+      except RuntimeError as err:
+        if re.match(err.args, "Conflicting edge detection already enabled for this GPIO channel"):
+          print "GPIO already configured elsewhere."
+        else raise err
+        
       GPIO.add_event_detect(self._gpio.flow_sensor, GPIO.RISING,
         callback=self._flow_counter.flow_rate_callback, bouncetime=100)
 
