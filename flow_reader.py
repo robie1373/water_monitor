53fa29e6405c16624c784cee64f9587c39b7241a
Robie Lutsey <robie1373@gmail.com>
2015-03-11 09:50:26 -0400

    Wooble on irc helped me fix the shared state prblem. Thank you Wooble.

--- flow_reader.py
+++ flow_reader.py
@@ -1,13 +1,24 @@
 class FlowReader():
-  def __init__(self, config, starter_set=[0]):
-    self._readings_set = starter_set
+  def __init__(self, config):
+    self._readings_set = [0]
     self._config = config
     self._moving_avg_interval = config.moving_avg_interval
     self._reading_interval = config.reading_interval
 
-  @property
-  def readings_set(self):
-    return self._readings_set
+  # @property
+  # def readings_set(self):
+  #   return self._readings_set
+
+  def readings_set():
+      doc = "The readings_set property."
+      def fget(self):
+          return self._readings_set
+      def fset(self, value):
+          self._readings_set = value
+      def fdel(self):
+          del self._readings_set
+      return locals()
+  readings_set = property(**readings_set())
 
   def take_reading(self, flow_counter):
     if len(self._readings_set) >= int(self._moving_avg_interval / self._reading_interval):
@@ -16,3 +27,4 @@ class FlowReader():
     # add new reading to end of array
     self._readings_set.append(flow_counter.flow_ticks)
     flow_counter.flow_ticks = 0
+
--- test_flow_reader.py
+++ test_flow_reader.py
@@ -2,6 +2,8 @@ import unittest
 from flow_reader import FlowReader
 from flow_counter import FlowCounter
 from controller_config import ControllerConfig
+from nose.tools import assert_equals
+from nose.tools import with_setup
 
 class TestFlowReader(unittest.TestCase):
   def setUp(self):
@@ -12,12 +14,6 @@ class TestFlowReader(unittest.TestCase):
     self.a_controller_config.reading_interval     = 1
     self.a_flow_reader            = FlowReader(self.a_controller_config)
 
-  def test_Readings_Set_Init(self):
-    self.assertEqual(self.a_flow_reader.readings_set, [0])
-
-  def test_Take_readings_adds_an_element(self):
-    self.a_flow_reader.take_reading(self.a_flow_counter)
-    self.assertEqual(self.a_flow_reader.readings_set, [0,5])
 
   def test_take_readings_limits_len_to_timeframe_over_interval(self):
     for _ in range(5):
@@ -48,6 +44,27 @@ class TestFlowReaderWithDefaultSettings(unittest.TestCase):
 
     self.assertEqual(len(self.a_flow_reader.readings_set), 
       int(self.a_controller_config.moving_avg_interval / self.a_controller_config.reading_interval) )
+
+class TestFlowReaderNose:
+  # @classmethod
+  def setUp(self):
+    self.a_flow_counter                           = FlowCounter()
+    self.a_flow_counter.flow_ticks                = 5
+    self.a_controller_config                      = ControllerConfig()
+    self.a_controller_config.moving_avg_interval  = 4
+    self.a_controller_config.reading_interval     = 1
+    self.a_flow_reader            = FlowReader(self.a_controller_config)
+
+  # @with_setup(setUpNose)
+  def test_Readings_Set_Init(self):
+    another_controller_config = ControllerConfig()
+    another_flow_reader = FlowReader(another_controller_config)
+    assert_equals(another_flow_reader.readings_set, [0])
+
+  # @with_setup(setUpNose)
+  def test_Take_readings_adds_an_element(self):
+    self.a_flow_reader.take_reading(self.a_flow_counter)
+    assert_equals(self.a_flow_reader.readings_set, [0,5])
 
 
 if __name__ == '__main__':
