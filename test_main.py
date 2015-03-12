2ffd992e6904909d9ee39d40b49dcb0899df08ab
Robie Lutsey <robie1373@gmail.com>
2015-03-11 20:53:36 -0400

    created new main and test_main. commented out old main tests.

---
+++ test_main.py
@@ -0,0 +1,6 @@
+from nose.tools import assert_equal
+import main
+
+class TestMain:
+  def setUp(self):
+    a_main = Main()
--- test_water_controller.py
+++ test_water_controller.py
@@ -1,46 +1,46 @@
-import threading
-import time
-from nose.tools import assert_equal
-from water_controller import Main
+# import threading
+# import time
+# from nose.tools import assert_equal
+# from water_controller import Main
 
-class TestMain():
-  def setUp(self):
-    self.a_main = Main()
+# class TestMain():
+#   def setUp(self):
+#     self.a_main = Main()
 
-  # def test_threaded_readings(self):
-  #   assert_equal(threading.active_count(), 1)
-  #   self.a_main.threaded_readings()
-  #   assert_equal(threading.active_count(), 2)
+#   # def test_threaded_readings(self):
+#   #   assert_equal(threading.active_count(), 1)
+#   #   self.a_main.threaded_readings()
+#   #   assert_equal(threading.active_count(), 2)
     
-  def test_what_config_looks_like_right_now(self):
-    assert_equal(self.a_main.test_config.moving_avg_interval, 300)
-    assert_equal(self.a_main.test_config.reading_interval, 5)
+#   def test_what_config_looks_like_right_now(self):
+#     assert_equal(self.a_main.test_config.moving_avg_interval, 300)
+#     assert_equal(self.a_main.test_config.reading_interval, 5)
 
-class TestMainTakeReading:
-  def setUp(self):
-    self.a_main = Main()
-    self.a_main.test_flow_reader.readings_set = [0,43,11]    
+# class TestMainTakeReading:
+#   def setUp(self):
+#     self.a_main = Main()
+#     self.a_main.test_flow_reader.readings_set = [0,43,11]    
     
-  def test_flow_reader_looks_sane(self):
-    assert dir(self.a_main.test_flow_reader).__contains__("take_reading")
+#   def test_flow_reader_looks_sane(self):
+#     assert dir(self.a_main.test_flow_reader).__contains__("take_reading")
 
-  def test_to_make_sure_FlowReader_take_reading_works_here(self):
-    orig_len = len(self.a_main.test_flow_reader.readings_set)
-    print "\norig readings set: ", self.a_main.test_flow_reader.readings_set
+#   def test_to_make_sure_FlowReader_take_reading_works_here(self):
+#     orig_len = len(self.a_main.test_flow_reader.readings_set)
+#     print "\norig readings set: ", self.a_main.test_flow_reader.readings_set
 
-    self.a_main.main_take_reading(96)
+#     self.a_main.main_take_reading(96)
 
-    new_len = len(self.a_main.test_flow_reader.readings_set)
-    print "new readings set: ", self.a_main.test_flow_reader.readings_set
+#     new_len = len(self.a_main.test_flow_reader.readings_set)
+#     print "new readings set: ", self.a_main.test_flow_reader.readings_set
     
-    assert_equal(new_len, orig_len + 1)
+#     assert_equal(new_len, orig_len + 1)
     
-  # def test_threaded_readings_appends_readings_set(self):
-  #   assert_equal(self.a_main.readings_set, [0])
-  #   self.a_main.threaded_readings()
-  #   time.sleep(7)
-  #   assert_equal(self.a_main.readings_set, [0,1])
+#   # def test_threaded_readings_appends_readings_set(self):
+#   #   assert_equal(self.a_main.readings_set, [0])
+#   #   self.a_main.threaded_readings()
+#   #   time.sleep(7)
+#   #   assert_equal(self.a_main.readings_set, [0,1])
 
 
-if __name__ == '__main__':
-  unittest.main()
+# if __name__ == '__main__':
+#   unittest.main()
