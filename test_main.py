1e03a815a6c2b420133ac10de697b7c9f1807810
Robie Lutsey <robie1373@gmail.com>
2015-03-11 23:21:08 -0400

    testable bugs squashed. time to try on rpi

--- test_main.py
+++ test_main.py
@@ -1,5 +1,7 @@
 from nose.tools import assert_equal
+from nose.tools import timed
 from main import Main
+import time
 
 class TestMain:
   def setUp(self):
@@ -15,3 +17,15 @@ class TestMain:
     assert_equal(self.a_main.flow_reader.readings_set, [0])
     self.a_main.flow_reader.take_reading(self.a_main.flow_counter.flow_ticks)
     assert_equal(self.a_main.flow_reader.readings_set, [0,0])
+
+  @timed(0.7)
+  def test_main_updates_reading_set_in_timed_thread(self):
+    self.a_main.config.reading_interval = 0.5
+    assert_equal(self.a_main.flow_reader.readings_set, [0])
+    self.a_main.flow_counter.flow_ticks = 8
+    self.a_main.threaded_readings()
+    time.sleep(0.6)
+    assert_equal(self.a_main.flow_reader.readings_set, [0,8])
+
+  def test_main_averages(self):
+    assert_equal(self.a_main.readings_calculator.calculate_average([8,4,6]), 6)
--- test_readings_calculator.py
+++ test_readings_calculator.py
@@ -4,10 +4,11 @@ from readings_calculator import ReadingsCalculator
 class TestReadingsCalculator(unittest.TestCase):
   def setUp(self):
     self.a_reading_set = [1,1,2,3,5,8,13]
-    self.a_readings_calculator = ReadingsCalculator(self.a_reading_set)
+    # self.a_readings_calculator = ReadingsCalculator(self.a_reading_set)
+    self.a_readings_calculator = ReadingsCalculator()
 
   def test_calculator_averages_a_list_of_ints(self):
-    self.assertEqual(self.a_readings_calculator.calculate_average(), int(4.7))
+    self.assertEqual(self.a_readings_calculator.calculate_average(self.a_reading_set), int(4.7))
 
 
 
